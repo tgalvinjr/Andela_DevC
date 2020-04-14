7321cfa2f006c444cb88acb7ec1f875b4578e0d5
tgalvinjr <tgalvin.jr@gmail.com>
2020-04-15 00:23:05 +0300

    completed challenge 1

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,23 @@
 def estimator(data):
-  return data
+    return data
+
+
+def covid19ImpactEstimator(reportedCases):
+    '''
+    '''
+    results = {
+        'data': {
+            reportedCases
+        },
+        'impact': {},
+        'severeImpact': {}
+    }
+    currentlyInfected = int(reportedCases) * 10
+    results['impact']['currentlyInfected'] = currentlyInfected
+    severeImpact = reportedCases * 50
+    results['severeImpact']['currentlyInfected'] = severeImpact
+    infectionsByRequestedTime = currentlyInfected * 512
+    results['impact'] = infectionsByRequestedTime
+    results['severeImpact'] = infectionsByRequestedTime
+
+    return results
